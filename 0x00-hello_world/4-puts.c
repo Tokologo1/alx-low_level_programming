1e23344511606e38df5599d00ed33ff7cbd279b9
Tokologo1 <tokocha81@gmail.com>
2022-09-08 15:13:05 -0700

    Fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
- * Main Write a Program
+
+/**
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a miltilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
