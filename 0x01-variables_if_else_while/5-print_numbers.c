632bff511a332edd37f1aca0b59d49d9b805e1ae
Tokologo1 <tokocha81@gmail.com>
2022-09-09 17:10:11 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		printf("%i", i);
+	}
+
+	print("\n");
+	return (0);
+}
