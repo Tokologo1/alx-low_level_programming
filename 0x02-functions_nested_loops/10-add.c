8083a73332c31303f87df001c630875a32135e58
Tokologo1 <tokocha81@gmail.com>
2022-09-13 16:40:29 -0700

    Task 10(Add)

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,2 +1,12 @@
 #include "main.h"
-
+/**
+ * add - Add two integers
+ * @one - The first integer to be added
+ * @two - The second integer to be added
+ *
+ * Return:Always 0 (Sucess)
+ */
+int add(int one , int two)
+{
+	return (one + two);
+}
