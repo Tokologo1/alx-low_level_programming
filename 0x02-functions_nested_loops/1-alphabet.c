ea424a9ef8ee6999ffe4efb3ce2ccb34ee0b2500
Tokologo1 <tokocha81@gmail.com>
2022-09-13 12:48:28 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+ * Display a lower case Alphabet and new line
+ *
+ * Return:Awalys 0 (Success)
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
