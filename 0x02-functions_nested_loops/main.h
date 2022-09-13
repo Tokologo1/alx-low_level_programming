bed0f1e016943ff8747a764580722fc82ad403bc
Tokologo1 <tokocha81@gmail.com>
2022-09-13 11:25:20 -0700

    Main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+char _putchar(char c);
+void print_alphabets(void);
+void print_alphabets_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int nId);
+void jack_bauer(void);
+void times_table;
+int add(int one, int two);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /*main_h*
