37c8aa011c5eaf063e4554661341ac7aa5c49fd1
Tokologo1 <tokocha81@gmail.com>
2022-09-13 17:02:44 -0700

    Task 1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_to_98 - function that prints all natural 
+* print_to_98 - function that prints all natural
 * numbers from n to 98, followed by a new line.
 * @n: the function will make use of the integer n
 * Return:Always 0 (Success)
