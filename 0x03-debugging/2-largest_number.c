8dda8ab03783c0adee930ef573283170b072d175
TKMshengu <tokocha81@gmail.com>
2022-09-15 15:15:43 +0200

    Fixing

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,31 +1,4 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@Tokologo1 
-Alouie412
-/
-holbertonschool-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-holbertonschool-low_level_programming/0x03-debugging/2-largest_number.c
-@Alouie412
-Alouie412 Condensed if/else statements
-Latest commit 3672e9d on Sep 24, 2019
- History
- 1 contributor
-45 lines (42 sloc)  727 Bytes
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
