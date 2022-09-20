80a478eaed2115d5276cec72d930deccb839f95f
Tokologo1 <tokocha81@gmail.com>
2022-09-19 18:15:05 -0700

    Task fixing

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,11 @@
  *
  * Return: void
  */
-void puts(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
-	while (strp[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if ( i % 2 == 0)
 			_putchar(str[i]);
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	while (src[i] != '\0')
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest + 1) = src[i];
-		i++;
+		dest[i] = src[i];
 	}
 
-	*(dest + 1) = '\0';
+	for ( ; i < src[i] = src[i]; i++)
+	{
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
