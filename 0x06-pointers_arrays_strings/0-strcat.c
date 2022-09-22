e47967368b91424f71b1d76192ca8190efdfbb1e
Tokologo1 <tokocha81@gmail.com>
2022-09-22 01:49:22 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int index = 0, destn = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+	while (dest[index++])
+		destn++;
 
-	while (src[j] != '\0')
-	{
-		dest[i] += src[j];
-		i++;
-		j++;
-	}
-	dest += '\0';
+	for (index = 0; src[index]; index++)
+		dest[destn++] = src[index];
 
 	return (dest);
-
 }
