84a8430826f5ba8ccfb1596b31a9f19a094dc17e
Tokologo1 <tokocha81@gmail.com>
2022-09-22 16:27:05 -0700

    Task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,16 @@
  */
 void print_number(int n)
 {
+	int num;
+	num = n;
 	if (n < 0)
 	{
-		n = -n;
+		num = -n;
 		_putchar('-');
 	}
-	if (n / 10)
+	if (num / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
