a5a7ff958793fe576e83a9c8ebd9507bf0cdcdd4
Tokologo1 <tokocha81@gmail.com>
2022-09-30 06:02:32 -0700

    Task 1-4

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: arry pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argc[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", agrv[i]);
+		i++;
+	}
+	return (0);
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main_h"
+/**
+ * main - Entry Point 
+ * atoi is a fuction that converts a string into an int
+ * @argc: arguments
+ * @argv: array poiting to arguments
+ * Return: Null
+ */
+int main(int argc, char *argv[])
+{
+	int i, red = 1;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error")
+			return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
